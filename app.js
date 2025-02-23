3d26b33292cef0e0358f15fedea81970236bc8a9
EnrickyAthaide <enrickyathaide@gmail.com>
2025-02-23 16:19:35 +0530

    setup

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express()
+const path = require("path")
+const cookieParser = require("cookie-parser")
+
 app.set("view engine" ,"ejs")
 
-
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 app.get("/" ,(req,res)=>{
     res.render("index")
+})
+app.get("/about" , (req , res)=>{
+    res.render("about")
 })
 
 
